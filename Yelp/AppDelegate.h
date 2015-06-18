d4dabe7bb469691489a5470ed16912ab9642a1c8
eternaltung <eternaltung@gmail.com>
2015-06-18 17:04:37 +0800

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Shih-Ming Tung on 6/18/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
